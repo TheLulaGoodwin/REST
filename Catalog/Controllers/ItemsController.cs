01f3f24182cfd70dc33fa943879532596d59400e
Lula Goodwin <33987325+TheLulaGoodwin@users.noreply.github.com>
2021-10-29 19:20:13 -0400

    add get item method to Items Controller

--- Catalog/Controllers/ItemsController.cs
+++ Catalog/Controllers/ItemsController.cs
@@ -1,3 +1,4 @@
+using System;
 using System.Collections.Generic;
 using Catalog.Entities;
 using Catalog.Repositories;
@@ -21,6 +22,13 @@ namespace Catalog.Controllers
         {
             var items = repository.GetItems();
             return items;
+        }
+        // GET /items/{id}
+        [HttpGet("{id}")]
+        public Item GetItem(Guid id)
+        {
+            var item = repository.GetItem(id);
+            return item;
         }
     }
 }
