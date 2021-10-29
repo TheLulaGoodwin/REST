643fbea88f137f3fc3ec11b6f88699544e27f8b5
Lula Goodwin <33987325+TheLulaGoodwin@users.noreply.github.com>
2021-10-29 19:02:58 -0400

    Add item.cs, and a controller and repo class

---
+++ Catalog/Controllers/ItemsController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.Controllers
+{
+    [ApiController]
+    [Route("items")]
+    public class ItemsController : ControllerBase
+    {
+        
+    }
+}
