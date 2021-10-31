4fd58e0823a7bf29c10a4daf68a2d30985525b59
Lula Goodwin <33987325+TheLulaGoodwin@users.noreply.github.com>
2021-10-31 15:03:17 -0400

    had healthchecks to db context

--- Catalog/Startup.cs
+++ Catalog/Startup.cs
@@ -34,11 +34,11 @@ namespace Catalog
         {
             BsonSerializer.RegisterSerializer(new GuidSerializer(BsonType.String));
             BsonSerializer.RegisterSerializer(new DateTimeOffsetSerializer(BsonType.String));
+            var mongoDbSettings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
 
             services.AddSingleton<IMongoClient>(ServiceProvider => 
             {
-                var settings = Configuration.GetSection(nameof(MongoDbSettings)).Get<MongoDbSettings>();
-                return new MongoClient(settings.ConnectionString);
+                return new MongoClient(mongoDbSettings.ConnectionString);
             });
 
             services.AddSingleton<IItemsRepository, MongoDbItemsRepository>();
@@ -51,7 +51,13 @@ namespace Catalog
                 c.SwaggerDoc("v1", new OpenApiInfo { Title = "Catalog", Version = "v1" });
             });
 
-            services.AddHealthChecks();
+            services.AddHealthChecks()
+                    .AddMongoDb(
+                        mongoDbSettings.ConnectionString, 
+                        name: "mongodb", 
+                        timeout: TimeSpan.FromSeconds(3),
+                        tags: new[] {"ready"}
+                        );
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
