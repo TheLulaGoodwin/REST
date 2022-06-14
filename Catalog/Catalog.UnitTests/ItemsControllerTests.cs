bfc9a06062482c4e3b04b266b1861199aec8581b
Lula Goodwin <33987325+TheLulaGoodwin@users.noreply.github.com>
2022-06-14 19:31:25 -0400

    added xunit packages and created getitemsasynctest
    create a GetItemsAsyncTest that tests if expected behavior happens when item is null

---
+++ Catalog/Catalog.UnitTests/ItemsControllerTests.cs
@@ -0,0 +1,32 @@
+using System;
+using Catalog.Api.Controllers;
+using Catalog.Api.Entities;
+using Catalog.Api.Repositories;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Moq;
+using Xunit;
+
+namespace Catalog.UnitTests;
+
+public class ItemsControllerTests
+{
+    [Fact]
+    public async Task GetItemAsync_WithUnexistingItem_ReturnsNotfound()
+    {
+            // Arrange
+            var repositoryStub = new Mock<IItemsRepository>();
+            repositoryStub.Setup(repo => repo.GetItemAsync(It.IsAny<Guid>()))
+                .ReturnsAsync((Item)null);
+                
+            var loggerStub = new Mock<ILogger<ItemsController>>();
+
+            var controller = new ItemsController(repositoryStub.Object, loggerStub.Object);
+
+            // Act
+            var result = await controller.GetItemAsync(Guid.NewGuid());
+
+            // Assert
+            Assert.IsType<NotFoundResult>(result.Result);
+    }
+}
